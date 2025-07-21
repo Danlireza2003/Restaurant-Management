77ca0b49f291949efeda96e6b285e4062c7ca79f
Danlireza2003 <69733671+Danlireza2003@users.noreply.github.com>
2025-07-21 18:22:48 -0300

    Update SecurityConfig.java

--- API-Login/src/main/java/com/example/API_Login/JWT/SecurityConfig.java
+++ API-Login/src/main/java/com/example/API_Login/JWT/SecurityConfig.java
@@ -44,4 +44,8 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
